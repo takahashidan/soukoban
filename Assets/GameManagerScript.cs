a6d1166de88937a4d78a7a71c3e68ce948c1c159
takahashidan <k023g1019@g.neec.ac.jp>
2024-05-09 10:54:25 +0900

    01

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int[] map;
+
     void Start()
     {
-        
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        string debugText = "";
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ",";
+        }
+
+        Debug.Log(debugText);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //‰E–îˆó
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            int playerIndex = -1;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex < map.Length - 1)
+            {
+                map[playerIndex + 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            string debugText = "";
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ", ";
+            }
+
+            Debug.Log(debugText);
+        }
+
+        //¶–îˆó
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int playerIndex = -1;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex > 0)
+            {
+                map[playerIndex - 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            string debugText = "";
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ", ";
+            }
+
+            Debug.Log(debugText);
+        }
+
     }
 }
